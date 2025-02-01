8fec94bb606711e2ce986c4028c13dfb06aade41
amadesya <154593967+amadesya@users.noreply.github.com>
2025-02-01 17:37:05 +0300

    triangle

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
-fun main() {
-    println("Hello World!")
+fun main(){
+    var person = Person("Чел", 34)
+    var jora = Student("Жора", 64, "ИСПП-35")
+    println(jora.toString())
+
+    person.outputInformation()
+    jora.outputInformation()
+
+    var triangle = Triangle(4,6,5)
+    println(triangle.name)
+    println(triangle.perimeter())
+    println(triangle.area())
+    triangle.figureInfo()
+    triangle.print()
 }
--- src/Triangle.kt
+++ src/Triangle.kt
@@ -1,23 +1,39 @@
-class Rectangle: IPrinter, IFigure {
-    var a: Int = 0
-    var b: Int = 0
-    var c: Int = 0
-    var h: Int = 0
+import kotlin.math.sqrt
+
+class Triangle(var a: Int, var b: Int, var c: Int): IPrinter, IFigure {
+    var p: Double= a.toDouble()+b.toDouble()+c.toDouble()/2
+    var h: Double = (2 / a.toDouble()) * sqrt(p * (p - a) * (p - b) * (p - c))
+
+    fun isExist(): Boolean{
+        return (a + b >= c) && (b + c >= a) && (c + a >= b)
+    }
 
     override fun print() {
-        println("${a}, ${b}, ${c}, ${h}")
+        if (isExist())
+            return println("${a}, ${b}, ${c}, ${h}")
+        else
+            return println("Треугольник не существует")
     }
 
-    override fun area(): Double {
-        return 0.5 * a * h
+    override fun area(): Any {
+        if (isExist())
+            return 0.5 * a * h
+        else
+            return println("Треугольник не существует")
     }
 
-    override fun perimeter(): Int {
-        return a + b + c
+    override fun perimeter(): Any{
+        if (isExist())
+            return a + b + c
+        else
+            return println("Треугольник не существует")
     }
 
     override fun figureInfo() {
-        println("Площадь: ${area()}, периметр: ${perimeter()}, стороны: ${a},${b},${c}")
+        if (isExist())
+            println("Фигура: ${name}, Площадь: ${area()}, периметр: ${perimeter()}, стороны: ${a},${b},${c}, высота: ${h}")
+        else
+            return println("Треугольник не существует")
     }
 
     override val name: String
